772ab6061c4540a61ec05cef7d77b0cec992f51f
James Lee <james@jamesl.dev>
2025-01-28 03:44:45 +0000

    feat: simple playable grand piano v1.0.0

---
+++ TDGSPGrandPiano/Contents/mods/TDGSPGrandPiano/media/lua/client/modifygrandpiano.lua
@@ -0,0 +1,15 @@
+-- Author: thedoorguy
+
+if not TDG then
+	TDG = {}
+end
+if not TDG.piano then
+	TDG.piano = {}
+end
+
+function TDG.piano.onLoadedTileDefinitions(ism)
+	ism:getSprite("recreational_01_40"):getProperties():Set("CustomName", "Piano")
+	ism:getSprite("recreational_01_41"):getProperties():Set("CustomName", "Piano")
+end
+
+Events.OnLoadedTileDefinitions.Add(TDG.piano.onLoadedTileDefinitions)
