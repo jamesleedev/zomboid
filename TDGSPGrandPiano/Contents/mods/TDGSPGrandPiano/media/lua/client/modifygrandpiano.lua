d14493797a3c239506133becd3b9f67718856e98
James Lee <james@jamesl.dev>
2025-01-29 21:44:58 +0000

    fix: update all piano orientations

--- TDGSPGrandPiano/Contents/mods/TDGSPGrandPiano/media/lua/client/modifygrandpiano.lua
+++ TDGSPGrandPiano/Contents/mods/TDGSPGrandPiano/media/lua/client/modifygrandpiano.lua
@@ -10,6 +10,8 @@ end
 function TDG.piano.onLoadedTileDefinitions(ism)
 	ism:getSprite("recreational_01_40"):getProperties():Set("CustomName", "Piano")
 	ism:getSprite("recreational_01_41"):getProperties():Set("CustomName", "Piano")
+	ism:getSprite("recreational_01_48"):getProperties():Set("CustomName", "Piano")
+	ism:getSprite("recreational_01_49"):getProperties():Set("CustomName", "Piano")
 end
 
 Events.OnLoadedTileDefinitions.Add(TDG.piano.onLoadedTileDefinitions)
